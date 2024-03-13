f837dfc9e7c49c286cdeedc8c408b8b8c08e73e8
thilak-gowda <119557881+thilak-gowda@users.noreply.github.com>
2024-03-13 18:33:18 +0530

    mobile responsivness

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import './slider.css';
+// import './slider.css';
 
 const Sidebar = () => {
   const location = useLocation();
